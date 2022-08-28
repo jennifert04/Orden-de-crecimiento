248baf7dea94e3dc5119ada8082d98886a79f60e
valeriapadilla <81450427+valeriapadilla@users.noreply.github.com>
2022-08-27 21:13:22 -0500

    Update Matriz.java

--- src/algoritmo/Matriz.java
+++ src/algoritmo/Matriz.java
@@ -25,44 +25,51 @@ public class Matriz {
     double temp1;
 
     public Matriz sumaMatrices(Matriz m2) {
-        
+        try{
+            if(matriz.length == m2.matriz.length && matriz[0].length == m2.matriz[0].length){
+            double[][] matrizSuma = new double[matriz.length][m2.matriz[0].length];
 
-        double[][] matrizSuma = new double[matriz.length][m2.matriz[0].length];
-        if(matriz.length == m2.matriz.length && matriz[0].length == m2.matriz[0].length){
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                double pos1 = matriz[i][j];
-                double pos2 = m2.matriz[i][j];
-                double suma = pos1 + pos2;
-                matrizSuma[i][j] = suma;
+            for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[i].length; j++) {
+                    double pos1 = matriz[i][j];
+                    double pos2 = m2.matriz[i][j];
+                    double suma = pos1 + pos2;
+                    matrizSuma[i][j] = suma;
+                }
             }
-        }
-        
-        }else{
-            System.out.println("No se puede realizar la suma");
-        }
-        Matriz suma = new Matriz(matrizSuma);
 
-        return suma;
-        
+            Matriz suma = new Matriz(matrizSuma);
+            return suma;
+
+            }
+            else
+                throw new Exception();            
+        }
+        catch(Exception e){
+            return null;
+        }        
     }
 
     public Matriz productoMatrices(Matriz m2) {
+        try{
+            if (matriz[0].length == m2.matriz.length) {
+                double[][] matrizProducto = new double[matriz.length][m2.matriz[0].length];
 
-        double[][] matrizProducto = new double[matriz.length][m2.matriz[0].length];
-        if (matriz[0].length == m2.matriz.length) {
-            for (int i = 0; i < matriz.length; i++) {
-                for (int j = 0; j < m2.matriz[0].length; j++) {
-                    for (int k = 0; k < matriz[0].length; k++) { //puede ser columnasA o filasB ya que deben ser iguales
-                        matrizProducto[i][j] += matriz[i][k] * m2.matriz[k][j];
+                for (int i = 0; i < matriz.length; i++) {
+                    for (int j = 0; j < m2.matriz[0].length; j++) {
+                        for (int k = 0; k < matriz[0].length; k++) { //puede ser columnasA o filasB ya que deben ser iguales
+                            matrizProducto[i][j] += matriz[i][k] * m2.matriz[k][j];
+                        }
                     }
                 }
+                Matriz producto = new Matriz(matrizProducto);
+                return producto;
+            }else{
+                throw new Exception();
             }
-        }else{
-            System.out.println("No se puede realizar el producto");
-        }
-        Matriz producto = new Matriz(matrizProducto);
-        return producto;
+        }catch(Exception e){
+            return null;}
+        
     }
 
     public boolean equals(Object o) {
@@ -76,58 +83,73 @@ public class Matriz {
         }
         return true;
     }
+    
+    public String toString(){
+        String string_matriz="";
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                string_matriz += matriz[i][j] + " ";
+            }
+            string_matriz+="\n";
+        }
+        return string_matriz;
+    }
 
     public Matriz CuadradoMagico(int n) {
-        double[][] cuadrado = new double[n][n];
-        if(n%2 == 0){
-        
-        
-        // Initialize position for 1
-        int i = n / 2;
-        int j = n - 1;
-        int orden = n * n;
-        // One by one put all values in magic square
-        for (int num = 1; num <= orden;) {
-            if (i == -1 && j == n) // 3rd condition
-            {
-                j = n - 2;
-                i = 0;
-            } else {
-                // 1st condition helper if next number
-                // goes to out of square's right side
-                if (j == n) {
-                    j = 0;
+        try{
+            if((n-1)%2 == 0){
+
+                double[][] cuadrado = new double[n][n];
+                // Initialize position for 1
+                int i = n / 2;
+                int j = n - 1;
+                int orden = n * n;
+
+                // One by one put all values in magic square
+                for (int num = 1; num <= orden;) {
+                    if (i == -1 && j == n) // 3rd condition
+                    {
+                        j = n - 2;
+                        i = 0;
+                    } else {
+                        // 1st condition helper if next number
+                        // goes to out of square's right side
+                        if (j == n) {
+                            j = 0;
+                        }
+
+                        // 1st condition helper if next number is
+                        // goes to out of square's upper side
+                        if (i < 0) {
+                            i = n - 1;
+                        }
+                    }
+
+                    // 2nd condition
+                    if (cuadrado[i][j] != 0) {
+                        j -= 2;
+                        i++;
+                        continue;
+                    } else // set number
+                    {
+                        cuadrado[i][j] = num++;
+                    }
+                    // 1st condition
+                    j++;
+                    i--;
                 }
+                Matriz cuadradomagico = new Matriz(cuadrado);
 
-                // 1st condition helper if next number is
-                // goes to out of square's upper side
-                if (i < 0) {
-                    i = n - 1;
-                }
-            }
-
-            // 2nd condition
-            if (cuadrado[i][j] != 0) {
-                j -= 2;
-                i++;
-                continue;
-            } else // set number
-            {
-                cuadrado[i][j] = num++;
-            }
-
-            // 1st condition
-            j++;
-            i--;
+                return cuadradomagico;
+            } 
+        else{
+            throw new Exception();
         }
-        }else{
-            System.out.println("No se puede realizar el cuadrado mágico");
         }
-        Matriz cuadradomagico = new Matriz(cuadrado);
-        
-        return cuadradomagico;
-        
-
+        catch(Exception e){
+            
+            return null;
+        }  
     }
 
     public static void main(String[] args) { //prueba unitaria suma y producto
@@ -153,6 +175,9 @@ public class Matriz {
             {25, 19, 13, 7, 1},
             {18, 12, 6, 5, 24},
             {11, 10, 4, 23, 17},};
+        
+        Stopwatch w = new Stopwatch();
+        
 
         Matriz matriz1 = new Matriz(m1);
 
@@ -171,7 +196,7 @@ public class Matriz {
         assert (matriz1.productoMatrices(matriz2).equals(matriz4));
 
         assert (mágico.CuadradoMagico(5).equals(cuadrado1));
-
     }
+
 
 }
